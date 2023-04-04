da3cd2804aca5507975cbafceb8d6fb8f3fcd907
Dee68 <ddimie283@gmail.com>
2023-04-04 20:44:52 +0000

    Add search functionality.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+                                render,
+                                get_object_or_404,
+                                redirect,
+                                reverse
+                            )
+from django.db.models import Q
 from .models import Product, Category
 
 # Create your views here.
@@ -6,7 +12,19 @@ from .models import Product, Category
 
 def all_products(request):
     products = Product.objects.all()
-    context = {'products': products}
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'You did not enter a search query criteria')
+                return redirect(reverse('products:products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+    products = Product.objects.filter(queries)
+    context = {
+                'products': products,
+                'search_term': query
+              }
     return render(request, 'products/products.html', context)
 
 
